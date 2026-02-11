4bb1f8f95c2f2857e035efb54dda5ce035531452
William Carvajal <williamchp998@gmail.com>
2026-02-10 21:45:51 -0500

    Restore conectarDB function

--- app/config/database.php
+++ app/config/database.php
@@ -1,18 +1,24 @@
 <?php
 
-$host = $_ENV['MYSQLHOST'];
-$port = $_ENV['MYSQLPORT'];
-$user = $_ENV['MYSQLUSER'];
-$pass = $_ENV['MYSQLPASSWORD'];
-$db   = $_ENV['MYSQLDATABASE'];
+function conectarDB() {
 
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$db;charset=utf8",
-        $user,
-        $pass
-    );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error BD: " . $e->getMessage());
+    $host = getenv('MYSQLHOST');
+    $port = getenv('MYSQLPORT');
+    $user = getenv('MYSQLUSER');
+    $pass = getenv('MYSQLPASSWORD');
+    $db   = getenv('MYSQLDATABASE');
+
+    try {
+        $pdo = new PDO(
+            "mysql:host=$host;port=$port;dbname=$db;charset=utf8",
+            $user,
+            $pass
+        );
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+
+    } catch (PDOException $e) {
+        die("Error BD: " . $e->getMessage());
+    }
 }
