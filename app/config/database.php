8a9fb5cff3e7c7249b3d8315332829c1803ad8d6
William Carvajal <williamchp998@gmail.com>
2026-02-10 21:24:26 -0500

    Use MYSQL_URL connection

--- app/config/database.php
+++ app/config/database.php
@@ -1,9 +1,13 @@
 <?php
-$host = getenv("MYSQLHOST");
-$port = getenv("MYSQLPORT");
-$db   = getenv("MYSQLDATABASE");
-$user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
+$url = getenv("MYSQL_URL");
+
+$parts = parse_url($url);
+
+$host = $parts['host'];
+$port = $parts['port'];
+$user = $parts['user'];
+$pass = $parts['pass'];
+$db   = ltrim($parts['path'], '/');
 
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
 
